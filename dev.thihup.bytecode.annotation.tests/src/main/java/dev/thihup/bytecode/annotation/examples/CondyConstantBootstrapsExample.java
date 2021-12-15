7931b0d9110adba839124163721fe5f8711289f7
Thiago Henrique Hupner <thihup@gmail.com>
2021-12-15 00:05:03 -0300

    Optimize imports

--- dev.thihup.bytecode.annotation.tests/src/main/java/dev/thihup/bytecode/annotation/examples/CondyConstantBootstrapsExample.java
+++ dev.thihup.bytecode.annotation.tests/src/main/java/dev/thihup/bytecode/annotation/examples/CondyConstantBootstrapsExample.java
@@ -1,11 +1,6 @@
 package dev.thihup.bytecode.annotation.examples;
 
 import dev.thihup.bytecode.annotation.core.Bytecode;
-import java.lang.invoke.CallSite;
-import java.lang.invoke.ConstantCallSite;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 
 public class CondyConstantBootstrapsExample {
 
--- dev.thihup.bytecode.annotation.tests/src/main/java/dev/thihup/bytecode/annotation/examples/IndySimple.java
+++ dev.thihup.bytecode.annotation.tests/src/main/java/dev/thihup/bytecode/annotation/examples/IndySimple.java
@@ -3,7 +3,6 @@ package dev.thihup.bytecode.annotation.examples;
 import dev.thihup.bytecode.annotation.core.Bytecode;
 import java.lang.invoke.CallSite;
 import java.lang.invoke.ConstantCallSite;
-import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
