12296052e0a77fcb4c1339b8cc3612ad1d0e8972
Thiago Henrique Hupner <thihup@gmail.com>
2021-12-14 23:35:20 -0300

    Remove the generated files and refactor the processor

--- dev.thihup.bytecode.annotation.processor/src/main/java/dev/thihup/bytecode/annotation/processor/BytecodeProcessor.java
+++ dev.thihup.bytecode.annotation.processor/src/main/java/dev/thihup/bytecode/annotation/processor/BytecodeProcessor.java
@@ -1,13 +1,17 @@
 package dev.thihup.bytecode.annotation.processor;
 
+import static java.util.stream.Collectors.toMap;
+
 import dev.thihup.bytecode.annotation.core.Bytecode;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.constant.ClassDesc;
-import java.lang.constant.ConstantDescs;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filer;
@@ -16,97 +20,92 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.JavaFileObject;
 import org.openjdk.asmtools.jasm.Main;
 
-@SupportedAnnotationTypes({
-    "*"
-})
+@SupportedAnnotationTypes("dev.thihup.bytecode.annotation.core.Bytecode")
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
-public class BytecodeProcessor extends AbstractProcessor {
+public final class BytecodeProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(
-            Bytecode.class);
-        elementsAnnotatedWith.forEach(this::processMethod);
+        Set<? extends Element> elementsAnnotatedWith =
+            roundEnv.getElementsAnnotatedWith(Bytecode.class);
+        try {
+            Path outputDirectory = Files.createTempDirectory("bytecodeProcessor");
+            for (Element element : elementsAnnotatedWith) {
+                processMethod(outputDirectory, element);
+            }
 
+            Files.walkFileTree(outputDirectory, new DeleteAllFilesVisitor());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return true;
     }
 
-    private void processMethod(Element element) {
-        try {
-            Filer filer = processingEnv.getFiler();
+    private void processMethod(Path outputDirectory, Element element) throws IOException {
+        Bytecode annotation = element.getAnnotation(Bytecode.class);
 
-            List<? extends AnnotationMirror> annotationMirrors = element.getAnnotationMirrors();
-            var entries =
-                annotationMirrors.get(0).getElementValues();
+        String className = annotation.className();
+        String classNameWithSlashes = className.replace('.', '/');
+        String jasmCode = generateJasmCode(annotation, classNameWithSlashes);
 
-            String className = "";
-            String methodBody = "";
-            String returnTypeDescriptor = "V";
-            String methodName = "invoke";
-            String methodSignature = "()V";
-            String classVersion = "61:0";
-            String jasmCode = "";
+        Path jasmFile = outputDirectory.resolve(className + ".jasm");
+        Files.writeString(jasmFile, jasmCode);
 
-            for (var entry : entries.entrySet()) {
-                ExecutableElement key = entry.getKey();
-                String value = entry.getValue().getValue().toString();
-                switch (key.toString()) {
-                    case "className()" -> className = value;
-                    case "value()" -> methodBody = value;
-                    case "methodName()" -> methodName = value;
-                    case "methodSignature()" -> methodSignature = value;
-                    case "classVersion()" -> classVersion = value;
-                    case "jasmCode()" -> jasmCode = value;
-                }
-            }
-            String formatted = jasmCode.isEmpty() ? """
-                class %s
-                 version %s  {
-                    static Method "%s":"%s" stack 100 locals 100 {
-                        %s
-                    }
-                }
-                """.formatted(className.replace('.', '/'),
-                classVersion,
-                methodName, methodSignature, methodBody) : jasmCode;
+        compile(outputDirectory, jasmFile);
 
-            JavaFileObject bytecode = filer.createClassFile(className, element);
-
-            Path outputDirectory = Files.createTempDirectory("bytecodeProcessor");
-            Path jasmFile = outputDirectory.resolve(className + ".jasm");
-            Files.writeString(jasmFile, formatted);
-
-            Main compiler = new Main(new PrintWriter(System.out), "jasm") {
-                {
-                    this.err = new PrintWriter(System.err);
-                }
-            };
-            boolean compile = compiler.compile(
-                new String[]{"-d", outputDirectory.toString(), jasmFile.toString()});
-            if (!compile) {
-                throw new RuntimeException("Compilation failed");
-            }
-
-            try (var outputStream = bytecode.openOutputStream()) {
-                Files.copy(outputDirectory.resolve(className.replace('.', '/').concat(".class")),
-                    outputStream);
-            }
-
-            System.out.println("Method");
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        Filer filer = processingEnv.getFiler();
+        JavaFileObject bytecode = filer.createClassFile(className, element);
+        try (var outputStream = bytecode.openOutputStream()) {
+            Path classFile = outputDirectory.resolve(classNameWithSlashes.concat(".class"));
+            Files.copy(classFile, outputStream);
         }
-
     }
 
+    private String generateJasmCode(Bytecode annotation, String classNameWithSlashes) {
+        return !annotation.jasmCode().isEmpty() ? annotation.jasmCode() : """
+            class %s
+             version %s  {
+                static Method "%s":"%s" stack 100 locals 100 {
+                    %s
+                }
+            }
+            """.formatted(classNameWithSlashes,
+            annotation.classVersion(),
+            annotation.methodName(), annotation.methodSignature(), annotation.value());
+    }
 
+    private void compile(Path outputDirectory, Path jasmFile) {
+        Main compiler = new Main(new PrintWriter(System.out), "jasm") {
+            {
+                this.err = new PrintWriter(System.err);
+            }
+        };
+        boolean compile = compiler.compile(
+            new String[]{"-d", outputDirectory.toString(), jasmFile.toString()});
+        if (!compile) {
+            throw new RuntimeException("Compilation failed");
+        }
+    }
+
+    private static class DeleteAllFilesVisitor extends SimpleFileVisitor<Path> {
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc)
+            throws IOException {
+            Files.deleteIfExists(dir);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+            throws IOException {
+            Files.deleteIfExists(file);
+            return FileVisitResult.CONTINUE;
+        }
+    }
 }
